b3f56948d13389716e3feccb49433acaba11c31b
Joao Pedro <jp.maximoo@gmail.com>
2023-04-09 23:51:26 -0300

    status code responses especificadas

--- src/main/java/br/com/apideteste/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/apideteste/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import aj.org.objectweb.asm.Opcodes;
 import br.com.apideteste.projeto.DAO.IUsuario;
 import br.com.apideteste.projeto.model.Usuario;
 
@@ -22,34 +24,41 @@ import br.com.apideteste.projeto.model.Usuario;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 	
-	@Autowired // Classe da interface com as operações do banco de dados
+	@Autowired // Classe da interface com as operações de banco de dados
 	private IUsuario dao;
 	
 	@GetMapping
-	public List<Usuario> listar() {
-		return (List<Usuario>) dao.findAll();
+	public ResponseEntity<List<Usuario>> listar() {
+		
+		//Criando a variavel lista do tipo lista de usuario que contem todos os usuarios do banco de dados
+		List<Usuario> lista = (List<Usuario>) dao.findAll()
+				;
+		// O ResponseEntity irá retornar o status 200 e a variavel lista no body
+		return ResponseEntity.status(200).body(lista);
 	}
 	
 	@GetMapping ("/{id}")
-	public Optional<Usuario> consultar (@PathVariable Integer id) {
-		return dao.findById(id);
+	public ResponseEntity<Optional<Usuario>> consultar (@PathVariable Integer id) {
+		Optional<Usuario> usuario = dao.findById(id);
+		return ResponseEntity.status(200).body(usuario);
 	}
 	
 	@PostMapping
-	public Usuario incluir (@RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> incluir (@RequestBody Usuario usuario) {
 		Usuario novoUsuario = dao.save(usuario);
-		return novoUsuario;
+		return ResponseEntity.status(201).body(novoUsuario);
 	}
 	
 	@PutMapping
-	public Usuario alterar (@RequestBody Usuario usuario) {
-		Usuario alterado = dao.save(usuario);
-		return alterado;
+	public ResponseEntity<Usuario> alterar (@RequestBody Usuario usuario) {
+		Usuario usuarioAlterado = dao.save(usuario);
+		return ResponseEntity.status(201).body(usuarioAlterado);
 	}
 	
 	@DeleteMapping ("/{id}")
-	public void deletar (@PathVariable Integer id) {
+	public ResponseEntity<?> deletar (@PathVariable Integer id) {
 		dao.deleteById(id);
+		return ResponseEntity.status(204).build();
 	}
 	
 }
